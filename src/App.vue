037aa9c188d822cfac7a7de7ac71cc4ec5bdd72d
胡惠影 <2330600478@qq.com>
2022-11-25 17:30:21 +0800

    feat: ref实现数据响应

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,35 @@
   <h1>app</h1>
   <p>name：{{name}}</p>
   <button @click="sayHello">say</button>
+  <br>
+  <button @click="changeName">change</button>
 </template>
 
 <script>
 // import {h} from 'vue'
+import {ref} from 'vue'
 export default {
   name: 'App',
   components: {},
   setup() {
     // 数据
-    let name = 'clara'
+    let name =ref('clara')
 
     // 方法
     function sayHello() {
       console.log(`Hi，${name}`)
     }
+    // 修改名字
+    function changeName() {
+      console.log(name)
+      name.value = 'aa'
+    }
 
     // 返回对象（常用）
     return {
       name,
-      sayHello
+      sayHello,
+      changeName
     }
 
     // 返回一个函数（渲染函数）
